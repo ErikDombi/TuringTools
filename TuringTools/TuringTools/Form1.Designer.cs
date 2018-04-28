e0f4f0432b5135a18454572814d4f9bdf0e9e56e
wickedlizerd <erikd02@gmail.com>
2018-04-27 22:42:29 -0400

    Initial Commit

---
+++ TuringTools/TuringTools/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace TuringTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Red;
+            this.pictureBox2.Location = new System.Drawing.Point(283, 120);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(8, 8);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseEnter += new System.EventHandler(this.pictureBox2_MouseEnter);
+            this.pictureBox2.MouseLeave += new System.EventHandler(this.pictureBox2_MouseLeave);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Red;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.Location = new System.Drawing.Point(863, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(624, 12);
+            this.trackBar1.Maximum = 9;
+            this.trackBar1.Minimum = 3;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(624, 79);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "VariableX Value";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.ForeColor = System.Drawing.Color.White;
+            this.checkBox2.Location = new System.Drawing.Point(624, 102);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(101, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "VariableY Value";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(604, 565);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(900, 600);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TuringTools/TuringTools/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TuringTools
+{
+    public partial class Form1 : Form
+    {
+        int boxSize = 20;
+        List<Point> drawPoints = new List<Point>();
+        Color _color = Color.Red;
+        int penSize = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(b))
+            {
+                for(int i = 0; i < pictureBox1.Width / boxSize; i++)
+                {
+                    g.DrawLine(new Pen(Color.Black), new Point(i * boxSize, 0), new Point(i * boxSize, pictureBox1.Height));
+                }
+                for (int j = 0; j < pictureBox1.Height / boxSize; j++)
+                {
+                    g.DrawLine(new Pen(Color.Black), new Point(0, j * boxSize), new Point(pictureBox1.Width, j * boxSize));
+                }
+            }
+            pictureBox1.BackgroundImage = b;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int xRnd = (int)Math.Round((double)e.X / boxSize) * boxSize;
+            int yRnd = (int)Math.Round((double)e.Y / boxSize) * boxSize;
+            Console.WriteLine(new Point(xRnd, yRnd).ToString());
+            pictureBox2.Left = xRnd - pictureBox2.Width / 2;
+            pictureBox2.Top = yRnd - pictureBox2.Height / 2;
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            //Cursor.Hide();
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            //Cursor.Show();
+        }
+
+        private void pictureBox2_MouseLeave(object sender, EventArgs e)
+        {
+            //Cursor.Hide();
+        }
+
+        private void pictureBox2_MouseEnter(object sender, EventArgs e)
+        {
+            //Cursor.Hide();
+        }
+
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            Draw(e);
+        }
+
+        public void Draw(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int xRnd = (int)Math.Round((double)(MousePosition.X - this.Left) / boxSize) * boxSize;
+                int yRnd = (int)Math.Round((double)(MousePosition.Y - this.Top) / boxSize) * boxSize;
+                drawPoints.Add(new Point(xRnd, yRnd));
+
+                Bitmap b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                if (drawPoints.Count > 1)
+                    using (Graphics g = Graphics.FromImage(b))
+                    {
+                        for (int i = 0; i < drawPoints.Count - 1; i++)
+                        {
+                            g.DrawLine(new Pen(_color, penSize), drawPoints[i], drawPoints[i + 1]);
+                        }
+                    }
+
+                pictureBox1.Image = b;
+
+                Point TopLeft = new Point(drawPoints.Select(x => x.X).Min(), drawPoints.Select(x => x.Y).Min());
+                Console.WriteLine("Current most TopLeft point in the shape is: " + TopLeft.ToString());
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            if (colorDialog1.Color != null)
+            {
+                _color = colorDialog1.Color;
+                pictureBox3.BackColor = _color;
+
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Draw(e);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            penSize = trackBar1.Value;
+        }
+
+        public void GenerateCode()
+        {
+            Point TopLeft = new Point(drawPoints.Select(x => x.X).Min(), drawPoints.Select(x => x.Y).Min());
+            Point BotRight = new Point(drawPoints.Select(x => x.X).Max(), drawPoints.Select(x => x.Y).Max());
+            Size bmpSize = new Size(BotRight.X - TopLeft.X, BotRight.Y - TopLeft.Y);
+            int yEffector = SystemInformation.BorderSize.Height;
+
+            string code = "";
+            code += $"import GUI\nsetscreen(\"graphics:{bmpSize.Width};{bmpSize.Height};nobuttonbar\")\n";
+            if (checkBox1.Checked)
+                code += "var imgX : int := 0\n";
+            if (checkBox2.Checked)
+                code += $"var imgY : int := 0\n";
+            for (int i = 0; i < drawPoints.Count - 1; i++)
+            {
+                int x1 = (drawPoints[i].X - TopLeft.X);
+                int y1 = bmpSize.Height - (drawPoints[i].Y - TopLeft.Y)- yEffector;
+                int x2 = (drawPoints[i + 1].X - TopLeft.X);
+                int y2 = bmpSize.Height - (drawPoints[i + 1].Y - TopLeft.Y) - yEffector;
+                if(checkBox1.Checked && checkBox2.Checked)
+                {
+                    code += $"drawline (imgX + {x1}, imgY + {y1}, imgX + {x2}, imgY + {y2}, 153)\n";
+                }
+                else if (checkBox1.Checked)
+                {
+                    code += $"drawline (imgX + {x1}, {y1}, imgX + {x2}, {y2}, 153)\n";
+                }
+                else if (checkBox2.Checked)
+                {
+                    code += $"drawline ({x1}, imgY + {y1}, {x2}, imgY + {y2}, 153)\n";
+                }
+                else
+                {
+                    code += $"drawline ({x1}, {y1}, {x2}, {y2}, 153)\n";
+                }
+                code = code.Replace("+ -", "- ");
+                code = code.Replace(" + 0", "");
+            }
+
+            Clipboard.SetText(code);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GenerateCode();
+        }
+    }
+}
