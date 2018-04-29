4bbd28e58a0bae2860a217d00cc9ff25150d52fa
wickedlizerd <erikd02@gmail.com>
2018-04-29 01:41:09 -0400

    Fixed issues & started commenting code

--- TuringTools/TuringTools/Form1.cs
+++ TuringTools/TuringTools/Form1.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/*
+
+########################### [ WARNING ] ###########################
+
+  THERE ARE SOME AWFUL CODING PRACTICES BELOW. I STRONGLY ADVISE
+            AGAINST USING MY CODE AS A "LEARNING POINT"
+
+###################################################################
+
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,13 +23,19 @@ namespace TuringTools
 {
     public partial class Form1 : Form
     {
-        int boxSize = 20;
-        List<Point> currentPoints = new List<Point>();
-        List<Point[]> allPoints = new List<Point[]>();
-        Color _color = Color.Red;
-        int penSize = 3;
-        int RGBDecimalCount = 1;
-        int CurrentPointsRectSizeXY = 6; //Preferably an even number
+        int boxSize = 20; //Size of Grid
+
+        List<Point> currentPoints = new List<Point>(); //Points of the current line being drawn
+
+        List<Point[]> allPoints = new List<Point[]>(); //List of point arrays that specify old line locations
+
+        Color _color = Color.Red; //Pen Color
+
+        int penSize = 3; //Width of pen
+
+        int RGBDecimalCount = 1; //How many decimal points it should produce when creating the custom color in turing.
+
+        int CurrentPointsRectSizeXY = 3; //Specifies width of points where the current line is being drawn. This is best left as 3.
 
         public Form1()
         {
@@ -26,6 +43,7 @@ namespace TuringTools
             DoubleBuffered = true;
         }
 
+        //SUMMARY: Draw Background Grid
         private void Form1_Load(object sender, EventArgs e)
         {
             Bitmap b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
@@ -44,6 +62,7 @@ namespace TuringTools
             _color = pictureBox3.BackColor;
         }
 
+        //SUMMARY: When the mouse moves, move the grid-cursor. 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
             Draw(new MouseEventArgs(MouseButtons.XButton1, 1, e.X, e.Y, 0));
@@ -58,48 +77,61 @@ namespace TuringTools
             Draw(e);
         }
 
+        //SUMMARY: Draws the points onto the grid
         public void Draw(MouseEventArgs e)
         {
+            //When a right-click in envoked, push the current link into history and start a new line.
             if (e.Button == MouseButtons.Right)
             {
+                //Only add the line to allPoints[] if there are more than 1 point (Renderer does not draw single points)
                 if (currentPoints.Count > 1)
                     allPoints.Add(currentPoints.ToArray());
                 currentPoints = new List<Point>();
             }
 
+            //Renderer
             if (e.Button == MouseButtons.Left || e.Button == MouseButtons.None || e.Button == MouseButtons.Right || e.Button == MouseButtons.XButton1)
             {
                 int xRnd = pictureBox2.Left + pictureBox2.Width / 2;
                 int yRnd = pictureBox2.Top + pictureBox2.Height / 2;
 
+                //Add current cursor position to current line
                 if (e.Button == MouseButtons.Left)
                     currentPoints.Add(new Point(xRnd, yRnd));
 
-
+                //Initiate a new Bitmap (Canvas size)
                 Bitmap b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
                 using (Graphics g = Graphics.FromImage(b))
                 {
-
+                    //Darken the X & Y grid-lines that the cursor is on
                     g.DrawLine(new Pen(Color.Black, 2), new Point(xRnd, 0), new Point(xRnd, pictureBox1.Height));
                     g.DrawLine(new Pen(Color.Black, 2), new Point(0, yRnd), new Point(pictureBox1.Width, yRnd));
 
-
+                    //if there is 2 or more points on the current line ( > 0 to prevent drawing a point) draw the line
                     if (currentPoints.Count > 1)
                     {
+                        //For each point, Draw a line to current point to next point (Does not execute for last element in array)
                         for (int i = 0; i < currentPoints.Count - 1; i++)
                         {
                             g.DrawLine(new Pen(_color, penSize), currentPoints[i], currentPoints[i + 1]);
+                            
+                            //Draws a point on verticies for current line
                             g.FillRectangle(new SolidBrush(Color.Black), currentPoints[i].X - CurrentPointsRectSizeXY / 2, currentPoints[i].Y - CurrentPointsRectSizeXY / 2, CurrentPointsRectSizeXY, CurrentPointsRectSizeXY);
                         }
                     }
+
+                    //For all points (excluding the currently active line), connect them (as long as they are in the same line)
                     foreach (var pointArray in allPoints)
                     {
+                        //Only draw the point(s) if there is more than 1. This should not be possible, but this is rather a failsafe.
                         if (pointArray.Length > 1)
                             for (int i = 0; i < pointArray.Length - 1; i++)
                             {
                                 g.DrawLine(new Pen(_color, penSize), pointArray[i], pointArray[i + 1]);
                             }
                     }
+
+                    //if there is 1 or more points, draw the last point & print distance (in pixels) from last point
                     if (currentPoints.Count > 0)
                     {
                         g.FillRectangle(new SolidBrush(Color.Black), currentPoints[currentPoints.Count - 1].X - CurrentPointsRectSizeXY / 2, currentPoints[currentPoints.Count - 1].Y - CurrentPointsRectSizeXY / 2, CurrentPointsRectSizeXY, CurrentPointsRectSizeXY);
@@ -136,13 +168,15 @@ namespace TuringTools
 
         public void GenerateCode()
         {
-            //DOING THIS MAY BREAK BEING ABLE TO CONTINUE A LINE AFTER GENERATING. LOOK FOR A FIX (Probably simple)
+            //Doing this probably breaks continuing lines after exporting. I'll look for a fix later (Probably simple)
+            //Edit: Fuck. I knew a fix while writing the comment above, but I forget now.
             if (currentPoints.Count > 1)
                 allPoints.Add(currentPoints.ToArray());
             currentPoints = new List<Point>();
 
-            Point TopLeft = new Point(allPoints.Select(x => x.Select(c => c.X).Min()).Min(), allPoints.Select(x => x.Select(c => c.Y).Min()).Min());
-            Point BotRight = new Point(allPoints.Select(x => x.Select(c => c.X).Max()).Max(), allPoints.Select(x => x.Select(c => c.Y).Max()).Max());
+            
+            Point TopLeft = new Point(allPoints.Select(x => x.Select(c => c.X).Min()).Min(), allPoints.Select(x => x.Select(c => c.Y).Min()).Min()); //CREATE POINT WITH LOWEST X CO-ORDINATE & LOWEST Y CO-ORDINATE
+            Point BotRight = new Point(allPoints.Select(x => x.Select(c => c.X).Max()).Max(), allPoints.Select(x => x.Select(c => c.Y).Max()).Max()); //CREATE POINT WITH GREATEST X CO-ORDINATE & GREATEST Y CO-ORDINATE
             Size bmpSize = new Size(BotRight.X - TopLeft.X, BotRight.Y - TopLeft.Y);
             int yEffector = SystemInformation.BorderSize.Height;
             string code = "";
@@ -268,5 +302,6 @@ namespace TuringTools
         {
             moving = false;
         }
+
     }
 }
